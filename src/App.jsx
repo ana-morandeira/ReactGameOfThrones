d33d7786ee9f78537fa4fbb7b1b35fe0e0f40c6e
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-02-04 19:49:51 +0100

    feat: add search functionality and cleanup configuration files

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,27 @@ function App() {
     loadData();
   }, []);
 
+  // Lógica de filtrado
+  const filteredCharacters = characters.filter((char) =>
+    char.fullName.toLowerCase().includes(search.toLowerCase())
+  );
+
  return (
   <div className="min-h-screen bg-gray-100 p-8">
     <h1 className="text-3xl font-bold text-center mb-10 text-slate-800">
       Game of Thrones API
     </h1>
+
+    {/* Buscador */}
+      <div className="max-w-md mx-auto mb-10">
+        <input
+          type="text"
+          placeholder="Search character..."
+          className="w-full p-3 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
     
     {/* Este es el contenedor que hace la magia del grid */}
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
@@ -31,6 +47,6 @@ function App() {
     </div>
   </div>
 );
-}git
+}
 
 export default App;
